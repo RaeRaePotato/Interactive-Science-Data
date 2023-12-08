6b89842e547db7b0be2aca946c39d72d6486127e
BradyLenox <151767352+BradyLenox@users.noreply.github.com>
2023-12-08 18:56:10 -0500

    Create create_graph.py

---
+++ create_graph.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+from pydub import AudioSegment
+import numpy as np
+
+def display_waveform(file_path):
+    audio = AudioSegment.from_file(file_path)
+    samples = np.array(audio.get_array_of_samples())
+    sample_rate = audio.frame_rate
+
+    time = np.arange(0, len(samples)) / sample_rate  # Calculate time array
+
+    plt.figure(figsize=(8, 6))
+    plt.plot(time, samples)
+    plt.title('Audio Waveform')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Amplitude')
+    plt.grid(True)
+    plt.tight_layout()
+
+    plt.show()
