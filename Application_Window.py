e94f0c1eacfa65ccf13b792c0fd4b589ad49442b
RaeRae <raethelamb@gmail.com>
2023-12-08 16:22:53 -0500

    Check for multiple channels and meta data to remove them code added

--- Application_Window.py
+++ Application_Window.py
@@ -4,65 +4,95 @@ from tkinter import filedialog as fd
 from tkinter.messagebox import showinfo
 from pydub import AudioSegment
 import os
-
+from mutagen import File
 
 class Model:
     def __init__(self):
         self.input_file = ''
+        self.gfile = ''
+        self.root = tk.Tk()
+        self.root.title('Tkinter Open File Dialog')
+        self.root.resizable(False, False)
+        self.root.geometry('500x250')
 
-    gfile = ''
-    root = tk.Tk()
-    root.title('Tkinter Open File Dialog')
-    root.resizable(False, False)
-    root.geometry('500x250')
+    def convert_to_wav(self, input_file):
+        _, input_extension = os.path.splitext(input_file)
+        if input_extension.lower() == '.wav':
+            return input_file, 'wav'  # Return the original .wav file and format
 
+        else:
+            audioclip = AudioSegment.from_file(input_file, format=input_extension[1:])
+            self.input_file = audioclip.export("UpdatedClap.wav", format="wav")
+            return "UpdatedClap.wav", input_extension[1:]  # Return the new .wav file and format
 
-def convert_to_wav(input_file):
-    _, input_extension = os.path.splitext(input_file)
-    if input_extension.lower() == '.wav':
-        return input_file, 'wav'  # Return the original .wav file and format
+    def remove_metadata_tags(self, input_file):
+        audio = File(input_file)
+        audio.delete()
+        audio.save()
 
-    else:
-        audioclip = AudioSegment.from_file(input_file, format=input_extension[1:])
-        Model.input_file = audioclip.export("UpdatedClap.wav", format="wav")
-        return "UpdatedClap.wav", input_extension[1:]  # Return the new .wav file and format
+    def select_file(self):
+        filetypes = (
+            ('Audio Files', ('*.wav', '.mp3')),
+            ('All files', '*.*')
+        )
 
+        filename = fd.askopenfilename(
+            title='Open a file',
+            initialdir='/',
+            filetypes=filetypes)
 
-def select_file():
-    filetypes = (
-        ('Audio Files', ('*.wav', '.mp3')),
-        ('All files', '*.*')
-    )
+        if filename:
+            _, file_extension = os.path.splitext(filename)
+            if file_extension.lower() == '.wav':
+                self.gfile, new_format = filename, 'wav'
+                showinfo(
+                    title='Selected File',
+                    message=f'Selected .wav file: {filename}'
+                )
+            else:
+                new_file, new_format = self.convert_to_wav(filename)
+                self.gfile = new_file
+                showinfo(
+                    title='File Converted',
+                    message=f'Selected file converted to .wav: {self.gfile}'
+                )
 
-    filename = fd.askopenfilename(
-        title='Open a file',
-        initialdir='/',
-        filetypes=filetypes)
+                # Remove metadata tags from the newly created file
+                self.remove_metadata_tags(self.gfile)
 
-    if filename:
-        _, file_extension = os.path.splitext(filename)
-        if file_extension.lower() == '.wav':
-            Model.gfile, new_format = filename, 'wav'
+            # Update gfile_label only once, outside of the if-else block
+            gfile_label = ttk.Label(self.root, text=f'{self.gfile} (Format: {new_format})')
+            gfile_label.pack(side="bottom")
+
+            self.remove_metadata_tags(self.gfile)
+
+    def single_channel(self):
+        if self.is_multi_chan(self.input_file):
+            audioclip = AudioSegment.from_wav(self.input_file).set_channels(1)
+            # Assuming you want to convert to wav and update input_file
+            self.input_file, new_format = self.convert_to_wav(audioclip)
             showinfo(
-                title='Selected File',
-                message=f'Selected .wav file: {filename}'
+                title='Single Channel',
+                message=f'Single channel applied. Updated file: {self.input_file} (Format: {new_format})'
             )
         else:
-            new_file, new_format = convert_to_wav(filename)
-            Model.gfile = new_file
             showinfo(
-                title='File Converted',
-                message=f'Selected file converted to .wav: {Model.gfile}'
+                title='Single Channel',
+                message='The input file does not have 2 channels. No action taken.'
             )
 
-        # Update gfile_label only once, outside of the if-else block
-        gfile_label = ttk.Label(Model.root, text=f'{Model.gfile} (Format: {new_format})')
-        gfile_label.pack(side="bottom")
+    def is_multi_chan(self, input_file):
+        audioclip = AudioSegment.from_file(input_file)
+        return audioclip.channels == 2
 
+    def run(self):
+        # open button
+        open_button = ttk.Button(self.root, text='Load a file', command=self.select_file)
+        open_button.pack(expand=True)
 
-# open button
-open_button = ttk.Button(Model.root, text='Load a file', command=select_file)
-open_button.pack(expand=True)
+        # run the application
+        self.root.mainloop()
 
-# run the application
-Model.root.mainloop()
+# Create an instance of the Model class and run the application
+model_instance = Model()
+model_instance.run()
