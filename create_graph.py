e1e38cbb145b9ba1d705f4369bcbdc6962923ae2
RaeRae <raethelamb@gmail.com>
2023-12-09 18:17:41 -0500

    Updated

--- create_graph.py
+++ create_graph.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 from pydub import AudioSegment
 import numpy as np
 
+
 def display_waveform(file_path):
     audio = AudioSegment.from_file(file_path)
     samples = np.array(audio.get_array_of_samples())
@@ -9,7 +10,7 @@ def display_waveform(file_path):
 
     time = np.arange(0, len(samples)) / sample_rate  # Calculate time array
 
-    plt.figure(figsize=(8, 6))
+    plt.figure(figsize=(6.5, 5.5))
     plt.plot(time, samples)
     plt.title('Audio Waveform')
     plt.xlabel('Time (s)')
