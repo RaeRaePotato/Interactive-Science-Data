23ed16b6ca5c507172af6339f3a8e8b58d8de076
BradyLenox <151767352+BradyLenox@users.noreply.github.com>
2023-12-10 13:41:40 -0500

    Edited to create the frequency graph as well as the waveform graph

--- create_graph.py
+++ create_graph.py
@@ -1,21 +1,56 @@
 # create_graph.py
-import matplotlib.pyplot as plt
 from pydub import AudioSegment
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import numpy as np
+from tkinter import messagebox, Toplevel
 
-def display_waveform(file_path):
-    audio = AudioSegment.from_file(file_path)
-    samples = np.array(audio.get_array_of_samples())
-    sample_rate = audio.frame_rate
+def create_waveform(root, file_path):
+    try:
+        audio = AudioSegment.from_file(file_path)
+        samples = np.array(audio.get_array_of_samples())
+        sample_rate = audio.frame_rate
+        time = np.arange(0, len(samples)) / sample_rate
 
-    time = np.arange(0, len(samples)) / sample_rate  # Calculate time array
+        fig, ax = plt.subplots(figsize=(7, 5))
+        ax.plot(time, samples)
+        ax.set_title('Audio Waveform')
+        ax.set_xlabel('Time (s)')
+        ax.set_ylabel('Amplitude')
+        ax.grid(True)
+        plt.tight_layout()
 
-    plt.figure(figsize=(6.5, 5.5))
-    plt.plot(time, samples)
-    plt.title('Audio Waveform')
-    plt.xlabel('Time (s)')
-    plt.ylabel('Amplitude')
-    plt.grid(True)
-    plt.tight_layout()
+        waveform_canvas = FigureCanvasTkAgg(fig, master=root)
+        waveform_canvas.draw()
+        waveform_canvas.get_tk_widget().pack()
 
-    plt.show()
+    except Exception as e:
+        messagebox.showerror("Waveform Display Error", f"Error displaying waveform: {e}")
+
+def create_frequency_graph(file_path):
+    try:
+        audio = AudioSegment.from_file(file_path)
+        samples = np.array(audio.get_array_of_samples())
+        sample_rate = audio.frame_rate
+
+        n = len(samples)
+        freq = np.fft.rfftfreq(n, d=1 / sample_rate)
+        fft = np.abs(np.fft.rfft(samples))
+
+        fig, ax = plt.subplots(figsize=(7, 5))
+        ax.plot(freq, fft)
+        ax.set_title('Frequency Spectrum')
+        ax.set_xlabel('Frequency (Hz)')
+        ax.set_ylabel('Amplitude')
+        ax.grid(True)
+        plt.tight_layout()
+
+        root_freq = Toplevel()
+        root_freq.title("Frequency Graph")
+        canvas = FigureCanvasTkAgg(fig, master=root_freq)
+        canvas.draw()
+        canvas.get_tk_widget().pack()
+
+    except Exception as e:
+        messagebox.showerror("Frequency Graph Display Error", f"Error displaying frequency graph: {e}")
+
